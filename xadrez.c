606a3d1a87f29211d8d661175dc29be701e583d7
Guidbuenos <guidbamello@gmail.com>
2025-09-17 17:39:14 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,46 +1,70 @@
 #include <stdio.h>
 #include <string.h>
+   //Recursivo para mover a torre
+    void movertorre(int casas) {
+    if (casas > 0) {
+      printf("Torre direita\n");
+      movertorre(casas - 1);
+    }
+    
+   }
+// Recursivo para mexer o Bispo para cima
+  void moverbispoc(int casas) {
+    if (casas > 0) {
+      printf("Bispo cima\n");
+      moverbispoc(casas - 1);
+    } }
+    // Recursivo para mexer o Bispo para a direita
+   void moverbispo(int casas){
+    if (casas > 0){ 
+      printf("Bispo direita\n");
+      moverbispo(casas - 1);
+    }
+    }
+    // Recursivo para mover a Rainha para a esquerda
+    void moverrainha(int casas){
+      if (casas > 0){
+        printf("Rainha esquerda\n");
+        moverrainha(casas - 1);
+      }
+    }
    
+
+
+
 int main() {
  
- int torre = 0, Rainha = 0, Bispo, Bispoc;
- int cavalob, cavalo = 0;
+ printf("Movimentando peças de xadrez!!!\n");
   
-  //movimento da torre usando loop do-while
-  do 
-  {
-    printf("Torre direita\n");
-    torre++;
-  }while(torre <= 4);
+ 
+ //movimento da torre usando recursividade
+ 
+  movertorre(5);
+ 
   printf("\n");
-  //movimento do bispo usando loop aninhado
+  
+  //movimento do bispo usando recursividade
 
-  for(Bispo = 1; Bispo <= 5; Bispo++)
-  {
-    for(Bispoc = 1; Bispoc <=1; Bispoc++){
-    printf("Bispo cima\n");
-  } 
-    printf("Bispo Direita\n");
-  }
+  moverbispoc(5);
+  moverbispo(5);
+  
   printf("\n");
-  //movimento da Rainha usando loop while
-  while (Rainha <= 8)
-  {
-   printf("Rainha esquerda\n");
-    Rainha++;
-    }
+
+  //movimento da Rainha usando recursividade
+
+  moverrainha(8);
+
   printf("\n");
+
    //movimento do cavalo usando loop aninhado
-  
-   for(cavalob = 1; cavalob <= 1; cavalob++) {
-        while(cavalo <= 1) {
-            printf("cavalo baixo\n");
-            cavalo++;
-        }
-        printf("cavalo esquerda\n");
-    } 
-  
 
+  for(int cavaloc = 0, cavalo = 1; cavalo <= 1; cavalo++) {
+       for(cavaloc = 0; cavaloc <= 1; cavaloc++)
+       {
+        printf("Cavalo cima\n");
+    }
+    printf("Cavalo Direita\n");
+  }
     return 0;
 
    }
